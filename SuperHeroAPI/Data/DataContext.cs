a467f8963d5b0e77cd8b6a9828cdf5e5013d6d02
Murat Kara <muratkarazade@gmail.com>
2022-07-18 03:27:53 +0300

    Implement the DataContext

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
